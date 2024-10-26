2ac442f9d6af6503a6498ab469de36fc9b42abae
hlewluv <153503120+hlewluv@users.noreply.github.com>
2024-10-26 14:26:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ def main():
         'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot',
         'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot',
         'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot',
-        'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'
     ]
 
     while True:
